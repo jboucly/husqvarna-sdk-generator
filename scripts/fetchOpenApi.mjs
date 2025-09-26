8eb66c8b538fa86b98973a5a681edac1c2ede0ca
Jboucly <julien_boucly@ext.carrefour.com>
2025-09-26 17:45:22 +0200

    feat(fetchSpec): update fetch spec script to get all specs from API swagger

--- scripts/fetchOpenApi.mjs
+++ scripts/fetchOpenApi.mjs
@@ -4,22 +4,31 @@ const https = require("https");
 require("dotenv").config();
 
 const scriptName = chalk.cyan("[ Fetch OpenAPI ] ");
-const API_URL = process.env.API_URL || "http://localhost:3333";
-const OPENAPI_PATH = "/swagger.yml";
 
-function fetchOpenApiSpec() {
+const AUTH_API_URL = `${process.env.AUTH_API_URL}`;
+const CONNECT_API_URL = `${process.env.CONNECT_API_URL}`;
+const AUTO_MOWER_API_URL = `${process.env.AUTO_MOWER_API_URL}`;
+
+function fetchOpenApiSpec(url, outputPath) {
     https
-        .get(`${API_URL}${OPENAPI_PATH}`, (res) => {
+        .get(url, (res) => {
             let data = "";
 
             res.on("data", (chunk) => (data += chunk));
 
             res.on("end", () => {
-                fs.writeFileSync("openapi.yml", data);
+                // Ensure the directory exists
+                const dir = require("path").dirname(outputPath);
+
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir, { recursive: true });
+                }
+
+                fs.writeFileSync(outputPath, data);
 
                 console.info(
                     scriptName,
-                    chalk.white("OpenAPI spec saved to openapi.yml")
+                    chalk.white(`OpenAPI spec saved to ${outputPath}`)
                 );
             });
         })
@@ -32,5 +41,16 @@ function fetchOpenApiSpec() {
         });
 }
 
+console.info(scriptName, chalk.white("Remove old OpenAPI specs..."));
+await spinner(chalk.gray("Waiting please..."), () => $`pnpm rimraf openApi`);
+
 console.info(scriptName, chalk.white("Fetching OpenAPI spec..."));
-await spinner(chalk.gray("Waiting please..."), () => fetchOpenApiSpec());
+await spinner(chalk.gray("Waiting please..."), () =>
+    fetchOpenApiSpec(AUTH_API_URL, "openApi/auth.yml")
+);
+await spinner(chalk.gray("Waiting please..."), () =>
+    fetchOpenApiSpec(CONNECT_API_URL, "openApi/connect.yml")
+);
+await spinner(chalk.gray("Waiting please..."), () =>
+    fetchOpenApiSpec(AUTO_MOWER_API_URL, "openApi/automower.yml")
+);
