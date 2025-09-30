ce5d4f06998fd70dcc65f68fd68750118e03b0c6
Jboucly <julien_boucly@ext.carrefour.com>
2025-09-30 16:26:13 +0200

    fix: update prettier ignore and refactor build-sdk and fetchOpenApi scripts for consistency

--- scripts/build-sdk.mjs
+++ scripts/build-sdk.mjs
@@ -1,172 +1,121 @@
 #!/usr/bin/env zx
-import "zx/globals";
+import 'zx/globals'
 
-import chalk from "chalk";
-import { config as SetupDotenv } from "dotenv";
-import fs from "fs";
-import path from "path";
-import { exit } from "process";
-import { spinner } from "zx";
+import chalk from 'chalk'
+import { config as SetupDotenv } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+import { exit } from 'process'
+import { spinner } from 'zx'
 
 /** ############################## DEFINE ATTRIBUTES ############################## */
 
-SetupDotenv();
-const enter = () => console.log("\n");
-const scriptName = chalk.cyan("[ Build SDK ] ");
+SetupDotenv()
+const enter = () => console.log('\n')
+const scriptName = chalk.cyan('[ Build SDK ] ')
 
 /** ############################## DEFINE FUNCTIONS ############################## */
 
 async function setTypeModuleAndUpdateTsConfig(name) {
-    const pkgPath = path.join(
-        __dirname,
-        "..",
-        "packages",
-        name,
-        "package.json"
-    );
-    const tsconfigPath = path.join(
-        __dirname,
-        "..",
-        "packages",
-        name,
-        "tsconfig.json"
-    );
+    const pkgPath = path.join(__dirname, '..', 'packages', name, 'package.json')
+    const tsconfigPath = path.join(__dirname, '..', 'packages', name, 'tsconfig.json')
 
     if (fs.existsSync(pkgPath)) {
-        const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
-        pkg.type = "module";
-        pkg.files = ["dist", "dist/esm"];
-        delete pkg.repository; // Remove existing repository field if any
+        const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
+        pkg.type = 'module'
+        pkg.files = ['dist', 'dist/esm']
+        delete pkg.repository // Remove existing repository field if any
         pkg.exports = {
-            ".": {
-                import: "./dist/esm/index.js",
-                require: "./dist/index.js",
+            '.': {
+                import: './dist/esm/index.js',
+                require: './dist/index.js'
             },
-            "./models/*": {
-                import: "./dist/esm/models/*.js",
-                require: "./dist/models/*.js",
+            './models/*': {
+                import: './dist/esm/models/*.js',
+                require: './dist/models/*.js'
             },
-            "./api/*": {
-                import: "./dist/esm/api/*.js",
-                require: "./dist/api/*.js",
-            },
-        };
+            './api/*': {
+                import: './dist/esm/api/*.js',
+                require: './dist/api/*.js'
+            }
+        }
 
-        fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+        fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
 
         console.info(
             scriptName,
-            `${chalk.green("✓")} ${chalk.white(
-                'Added "type": "module" and "files": ["dist"] to package.json'
-            )}`
-        );
+            `${chalk.green('✓')} ${chalk.white('Added "type": "module" and "files": ["dist"] to package.json')}`
+        )
     } else {
-        console.error(scriptName, chalk.red("package.json not found"));
-        exit(1);
+        console.error(scriptName, chalk.red('package.json not found'))
+        exit(1)
     }
 
     if (fs.existsSync(tsconfigPath)) {
-        const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, "utf8"));
+        const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf8'))
 
-        tsconfig.compilerOptions = tsconfig.compilerOptions || {};
-        tsconfig.compilerOptions.module = "ESNext";
+        tsconfig.compilerOptions = tsconfig.compilerOptions || {}
+        tsconfig.compilerOptions.module = 'ESNext'
 
-        fs.writeFileSync(tsconfigPath, JSON.stringify(tsconfig, null, 2));
+        fs.writeFileSync(tsconfigPath, JSON.stringify(tsconfig, null, 2))
 
-        console.info(
-            scriptName,
-            `${chalk.green("✓")} ${chalk.white(
-                "Modified module in tsconfig.json"
-            )}`
-        );
+        console.info(scriptName, `${chalk.green('✓')} ${chalk.white('Modified module in tsconfig.json')}`)
     } else {
-        console.error(scriptName, chalk.red("tsconfig.json not found"));
-        exit(1);
+        console.error(scriptName, chalk.red('tsconfig.json not found'))
+        exit(1)
     }
 }
 
 /** ################################# SCRIPT ################################## */
 
 try {
-    enter();
-    console.info(scriptName, chalk.white("Removing previous SDK..."));
-    await spinner(
-        chalk.gray("Waiting please..."),
-        () => $`pnpm rimraf packages`
-    );
+    enter()
+    console.info(scriptName, chalk.white('Removing previous SDK...'))
+    await spinner(chalk.gray('Waiting please...'), () => $`pnpm rimraf packages`)
 
-    console.info(
-        scriptName,
-        chalk.white("Generating husqvarna-authentication-sdk...")
-    );
+    console.info(scriptName, chalk.white('Generating husqvarna-authentication-sdk...'))
     await spinner(
-        chalk.gray("Waiting please..."),
-        () =>
-            $`openapi-generator-cli generate --generator-key husqvarna-authentication-sdk`
-    );
+        chalk.gray('Waiting please...'),
+        () => $`openapi-generator-cli generate --generator-key husqvarna-authentication-sdk`
+    )
 
-    console.info(scriptName, chalk.white("Post generation set module..."));
-    await spinner(chalk.gray("Waiting please..."), () =>
-        setTypeModuleAndUpdateTsConfig("husqvarna-authentication-sdk")
-    );
-    console.info(
-        scriptName,
-        chalk.white("Installing dependencies and building...")
-    );
+    console.info(scriptName, chalk.white('Post generation set module...'))
+    await spinner(chalk.gray('Waiting please...'), () => setTypeModuleAndUpdateTsConfig('husqvarna-authentication-sdk'))
+    console.info(scriptName, chalk.white('Installing dependencies and building...'))
     await spinner(
-        chalk.gray("Waiting please..."),
-        () =>
-            $`cd packages/husqvarna-authentication-sdk && pnpm install && pnpm build`
-    );
+        chalk.gray('Waiting please...'),
+        () => $`cd packages/husqvarna-authentication-sdk && pnpm install && pnpm build`
+    )
 
-    enter();
-    console.info(
-        scriptName,
-        chalk.white("Generating automower-connect-sdk...")
-    );
+    enter()
+    console.info(scriptName, chalk.white('Generating automower-connect-sdk...'))
     await spinner(
-        chalk.gray("Waiting please..."),
-        () =>
-            $`openapi-generator-cli generate --generator-key automower-connect-sdk`
-    );
-    console.info(scriptName, chalk.white("Post generation set module..."));
-    await spinner(chalk.gray("Waiting please..."), () =>
-        setTypeModuleAndUpdateTsConfig("automower-connect-sdk")
-    );
+        chalk.gray('Waiting please...'),
+        () => $`openapi-generator-cli generate --generator-key automower-connect-sdk`
+    )
+    console.info(scriptName, chalk.white('Post generation set module...'))
+    await spinner(chalk.gray('Waiting please...'), () => setTypeModuleAndUpdateTsConfig('automower-connect-sdk'))
 
-    console.info(
-        scriptName,
-        chalk.white("Installing dependencies and building...")
-    );
+    console.info(scriptName, chalk.white('Installing dependencies and building...'))
     await spinner(
-        chalk.gray("Waiting please..."),
+        chalk.gray('Waiting please...'),
         () => $`cd packages/automower-connect-sdk && pnpm install && pnpm build`
-    );
+    )
 
-    enter();
-    console.info(
-        scriptName,
-        chalk.white("Generating husqvarna-connectivity-sdk...")
-    );
+    enter()
+    console.info(scriptName, chalk.white('Generating husqvarna-connectivity-sdk...'))
     await spinner(
-        chalk.gray("Waiting please..."),
-        () =>
-            $`openapi-generator-cli generate --generator-key husqvarna-connectivity-sdk`
-    );
-    console.info(scriptName, chalk.white("Post generation set module..."));
-    await spinner(chalk.gray("Waiting please..."), () =>
-        setTypeModuleAndUpdateTsConfig("husqvarna-connectivity-sdk")
-    );
+        chalk.gray('Waiting please...'),
+        () => $`openapi-generator-cli generate --generator-key husqvarna-connectivity-sdk`
+    )
+    console.info(scriptName, chalk.white('Post generation set module...'))
+    await spinner(chalk.gray('Waiting please...'), () => setTypeModuleAndUpdateTsConfig('husqvarna-connectivity-sdk'))
 
-    console.info(
-        scriptName,
-        chalk.white("Installing dependencies and building...")
-    );
+    console.info(scriptName, chalk.white('Installing dependencies and building...'))
     await spinner(
-        chalk.gray("Waiting please..."),
-        () =>
-            $`cd packages/husqvarna-connectivity-sdk && pnpm install && pnpm build`
-    );
+        chalk.gray('Waiting please...'),
+        () => $`cd packages/husqvarna-connectivity-sdk && pnpm install && pnpm build`
+    )
 } catch (e) {
-    console.info(chalk.red(`[ Build SDK ] Error: ${e}`));
+    console.info(chalk.red(`[ Build SDK ] Error: ${e}`))
 }
--- scripts/fetchOpenApi.mjs
+++ scripts/fetchOpenApi.mjs
@@ -1,56 +1,49 @@
-const fs = require("fs");
-const https = require("https");
+const fs = require('fs')
+const https = require('https')
 
-require("dotenv").config();
+require('dotenv').config()
 
-const scriptName = chalk.cyan("[ Fetch OpenAPI ] ");
+const scriptName = chalk.cyan('[ Fetch OpenAPI ] ')
 
-const AUTH_API_URL = `${process.env.AUTH_API_URL}`;
-const CONNECT_API_URL = `${process.env.CONNECT_API_URL}`;
-const AUTO_MOWER_API_URL = `${process.env.AUTO_MOWER_API_URL}`;
+const AUTH_API_URL = `${process.env.AUTH_API_URL}`
+const CONNECT_API_URL = `${process.env.CONNECT_API_URL}`
+const AUTO_MOWER_API_URL = `${process.env.AUTO_MOWER_API_URL}`
 
 function fetchOpenApiSpec(url, outputPath) {
     https
         .get(url, (res) => {
-            let data = "";
+            let data = ''
 
-            res.on("data", (chunk) => (data += chunk));
+            res.on('data', (chunk) => (data += chunk))
 
-            res.on("end", () => {
+            res.on('end', () => {
                 // Ensure the directory exists
-                const dir = require("path").dirname(outputPath);
+                const dir = require('path').dirname(outputPath)
 
                 if (!fs.existsSync(dir)) {
-                    fs.mkdirSync(dir, { recursive: true });
+                    fs.mkdirSync(dir, { recursive: true })
                 }
 
-                fs.writeFileSync(outputPath, data);
+                fs.writeFileSync(outputPath, data)
 
-                console.info(
-                    scriptName,
-                    chalk.white(`OpenAPI spec saved to ${outputPath}`)
-                );
-            });
+                console.info(scriptName, chalk.white(`OpenAPI spec saved to ${outputPath}`))
+            })
         })
-        .on("error", (err) => {
-            console.error(
-                scriptName,
-                chalk.red("Error fetching OpenAPI spec :"),
-                err
-            );
-        });
+        .on('error', (err) => {
+            console.error(scriptName, chalk.red('Error fetching OpenAPI spec :'), err)
+        })
 }
 
-console.info(scriptName, chalk.white("Remove old OpenAPI specs..."));
-await spinner(chalk.gray("Waiting please..."), () => $`pnpm rimraf openApi`);
+console.info(scriptName, chalk.white('Remove old OpenAPI specs...'))
+await spinner(chalk.gray('Waiting please...'), () => $`pnpm rimraf openApi`)
 
-console.info(scriptName, chalk.white("Fetching OpenAPI spec..."));
-await spinner(chalk.gray("Waiting please..."), () =>
-    fetchOpenApiSpec(AUTH_API_URL, "openApi/husqvarna-authentication.yml")
-);
-await spinner(chalk.gray("Waiting please..."), () =>
-    fetchOpenApiSpec(CONNECT_API_URL, "openApi/husqvarna-connectivity.yml")
-);
-await spinner(chalk.gray("Waiting please..."), () =>
-    fetchOpenApiSpec(AUTO_MOWER_API_URL, "openApi/automower-connect.yml")
-);
+console.info(scriptName, chalk.white('Fetching OpenAPI spec...'))
+await spinner(chalk.gray('Waiting please...'), () =>
+    fetchOpenApiSpec(AUTH_API_URL, 'openApi/husqvarna-authentication.yml')
+)
+await spinner(chalk.gray('Waiting please...'), () =>
+    fetchOpenApiSpec(CONNECT_API_URL, 'openApi/husqvarna-connectivity.yml')
+)
+await spinner(chalk.gray('Waiting please...'), () =>
+    fetchOpenApiSpec(AUTO_MOWER_API_URL, 'openApi/automower-connect.yml')
+)
